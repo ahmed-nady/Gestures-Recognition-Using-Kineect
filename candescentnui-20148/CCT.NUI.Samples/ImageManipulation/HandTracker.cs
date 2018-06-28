83777b4e5c62746688a893ec7b1333b9ac0ac0e4
ahmed-nady <ahmednady@fci.helwan.edu.eg>
2018-06-28 21:28:07 +0300

    Sumbit Final Changes

---
+++ candescentnui-20148/CCT.NUI.Samples/ImageManipulation/HandTracker.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CCT.NUI.Core;
+using CCT.NUI.HandTracking;
+using OpenNI;
+
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+using Recognizer.NDollar;
+namespace CCT.NUI.Samples.ImageManipulation
+{
+    public class HandTracker
+    {
+        private bool isDragging = false;
+        private bool isResizing = false;
+
+        private Point startDragPoint;
+        private Point startDragPoint2;
+
+        private InteractiveImage hoveredImage;
+
+        private HandData handData;
+
+
+        List<PointR> TargetsListofPoints = new List<PointR>();
+        List<PointR>  twoFingerPoints = new List<PointR>();
+
+
+        int missedSuccessiveFrame = 0;
+        string lastRecognizedGesture = "";
+
+        public HandTracker(HandData handData)
+        {
+            this.handData = handData;
+        }
+
+        public int Id { get { return this.handData.Id; } }
+
+        public void SetHandData(HandData newData)
+        {
+            this.handData = newData;
+        }
+        public InteractiveImage HoveredImage 
+        {
+            get { return this.hoveredImage; }
+        }
+
+        // Get a handle to an application window.
+        [DllImport("USER32.DLL", CharSet = CharSet.Unicode)]
+        public static extern IntPtr FindWindow(string lpClassName,
+            string lpWindowName);
+
+        // Activate an application window.
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        public void controlModel(String gesture, String commands)
+        {
+            // Get a handle to the ImageJ 3D Viewer application. The window class 
+            // and window name were obtained using the Spy++ tool.
+            // IntPtr calculatorHandle = FindWindow("CalcFrame", "Calculator");
+            IntPtr ImageJHandle = FindWindow("SunAwtFrame", "ImageJ 3D Viewer");
+
+
+            // Verify that ImageJ 3D Viewer is a running process. 
+            if (ImageJHandle == IntPtr.Zero)
+            {
+                System.Windows.Forms.MessageBox.Show("ImageJ 3D Viewer is not running.");
+                return;
+            }
+            //  if(gesture =="zoom")
+            //       runCommand("/c java -jar D:\\Working\\ImageJ\\ij.jar -macro rotate");
+            //// Make ImageJ 3D Viewer the foreground application and send it  
+            // a set of commands.
+            SetForegroundWindow(ImageJHandle);
+
+            // SendKeys.SendWait("{NUMLOCK}");
+            //arrow right {RIGHT} ,arrow left {LEFT} {DOWN} {UP} for rotation
+            //+{LEFT} for translation
+            // for (int i = 0; i < 20; i++)
+            //     SendKeys.SendWait(commands);
+            SendKeys.SendWait(commands);
+
+
+        }
+        public void HandleTranslation(InteractiveImage image, float zoomFactory)
+        {
+            this.hoveredImage = image;
+            hoveredImage.Hovered = true;
+
+            if (isResizing)
+            {
+                return;
+            }
+            var handClosed = handData.FingerCount <= 1;
+            if (isDragging)
+            {
+
+               
+
+
+              ////  hoveredImage.Translate((handData.PalmPoint.Value.X - startDragPoint.X) * zoomFactory, (handData.PalmPoint.Value.Y - startDragPoint.Y) * zoomFactory);
+              //  //deteremine the direction of translation
+              //  float dx = (handData.PalmPoint.Value.X - startDragPoint.X);
+              //  float dy = (handData.PalmPoint.Value.Y - startDragPoint.Y);
+              //  //move from right to left in x direction ,top to down in y direction
+              //  if (dx > 0 && dy > 0)
+              //  {
+              //      controlModel("translate", "+{LEFT 3}");
+              //      controlModel("translate", "+{DOWN 3}");
+
+              //  }
+              //  else if (dx > 0 && dy <0)
+              //  {
+              //      controlModel("translate", "+{LEFT 3}");
+              //      controlModel("translate", "+{UP 3}");
+              //  }
+              //  else if (dx < 0 && dy < 0)
+              //  {
+              //      controlModel("translate", "+{RIGHT 3}");
+              //      controlModel("translate", "+{UP 3}");
+              //  }
+              //  else
+              //  {
+              //      controlModel("translate", "+{RIGHT 10}");
+              //      controlModel("translate", "+{DOWN 10}");
+              //  }
+
+            }
+            if (handClosed)
+            {
+                startDragPoint = new Point(handData.FingerPoints[0].Location.X, handData.FingerPoints[0].Location.Y, 0);
+                TargetsListofPoints.Add(new PointR(handData.FingerPoints[0].X, handData.FingerPoints[0].Y, Environment.TickCount));
+                    
+            }
+            isDragging = handClosed;
+        }
+
+        private void recognizeGestureUsingNRecognizer(GeometricRecognizer recognizser, List<PointR> TargetsListofPoints)
+        {
+
+            NBestList result = recognizser.Recognize(TargetsListofPoints, 1);
+            if (result.Score > .7)
+            {
+                if (result.Name.StartsWith("TranslateRight"))
+                {
+                    controlModel("translate", "+{RIGHT 10}");
+                    lastRecognizedGesture = "TranslateRight";
+                }
+                else if (result.Name.StartsWith("TranslateLeft"))
+                {
+                    controlModel("translate", "+{LEFT 10}");
+                    lastRecognizedGesture = "TranslateLeft";
+                }
+                else if (result.Name.StartsWith("ZoomIn"))
+                {
+                    controlModel("Zoom", "%{UP 10}");
+                    lastRecognizedGesture = "ZoomIn";
+                }
+                else if (result.Name.StartsWith("ZoomOut"))
+                {
+                    controlModel("Zoom", "%{DOWN 10}");
+                    lastRecognizedGesture = "ZoomOut";
+                }
+                else if (result.Name.StartsWith("RotateLeft"))
+                {
+                    controlModel("Rotate", "{LEFT 10}");
+                    lastRecognizedGesture = "RotateLeft";
+                }
+                else if (result.Name.StartsWith("RotateRight"))
+                {
+                    controlModel("Rotate", "{RIGHT 10}");
+                    lastRecognizedGesture = "RotateRight";
+                }
+                else if (result.Name.StartsWith("ZoomIn2Fingers"))
+                {
+                    controlModel("Zoom", "%{UP 10}");
+                }
+                else if (result.Name.StartsWith("ZoomOut2Fingers"))
+                {
+                    controlModel("Zoom", "%{DOWN 10}");
+                }
+
+
+                TargetsListofPoints.Clear();
+
+            }
+        }
+     
+
+        public void HandleTranslation(GeometricRecognizer rec)
+        {
+            
+            if (isResizing)
+            {
+                return;
+            }
+            var handWithSingleFinger = handData.FingerCount == 1;
+            missedSuccessiveFrame = handWithSingleFinger ? 0 : missedSuccessiveFrame++;
+
+            if (missedSuccessiveFrame > 10 )//&& TargetsListofPoints.Count <20)
+            {
+                TargetsListofPoints.Clear();
+                missedSuccessiveFrame = 0;
+            }
+
+            if (handWithSingleFinger)
+            {
+                TargetsListofPoints.Add(new PointR(handData.FingerPoints[0].X, handData.FingerPoints[0].Y, Environment.TickCount));
+                //TargetsListofPoints.Add(new PointR(handData.PalmX, handData.PalmY, Environment.TickCount));
+            }
+                // recognize gesture when user pause for 10 ms
+                if (handWithSingleFinger==false && TargetsListofPoints.Count > 30)
+                {
+                        recognizeGestureUsingNRecognizer(rec,TargetsListofPoints);
+                        TargetsListofPoints.Clear();
+                }
+            if(handData.FingerCount >= 2)
+                performLastGestureNumberOfTimes();
+
+            }
+            //if (handWithSingleFinger)
+            //{
+            //    startDragPoint = new Point(handData.FingerPoints[0].Location.X, handData.FingerPoints[0].Location.Y, 0);
+            //    TargetsListofPoints.Add(new PointR(handData.FingerPoints[0].X, handData.FingerPoints[0].Y, Environment.TickCount));
+
+            //   // startDragPoint = new Point(handData.PalmX, handData.PalmY, 0);
+            //  //  TargetsListofPoints.Add(new PointR(handData.PalmX, handData.PalmY, Environment.TickCount));
+            
+            //}
+            //isDragging = handWithSingleFinger;
+       // }
+
+        public float distanceEuclidean(Point a, Point b)
+        {
+            float d = (float)Math.Sqrt(Math.Abs(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2)));
+            return d;
+        }
+        //perform the last gesture #of times ==fingertips.count
+        public void performLastGestureNumberOfTimes()
+        {
+            if (handData.FingerCount >= 2)
+            {
+                switch (lastRecognizedGesture)
+                {
+                    case "TranslateLeft": { controlModel("translate", "+{LEFT "+handData.FingerCount+"}"); break; }
+                    case "TranslateRight": { controlModel("translate", "+{RIGHT " +  handData.FingerCount + "}"); break; }
+                    case "RotateLeft": { controlModel("Rotate", "{LEFT "+  handData.FingerCount + "}"); break; }
+                    case "RotateRight": { controlModel("Rotate", "{RIGHT " +  handData.FingerCount + "}"); break; }
+                    case "ZoomIn": {  controlModel("Rotate", "%{UP " +  handData.FingerCount + "}"); break; }
+                    case "ZoomOut": { controlModel("Rotate", "%{DOWN " +  handData.FingerCount + "}"); break; } 
+                    default: break;
+                }
+            }
+        }
+        public void ResizeSingleHand(GeometricRecognizer rec)
+        {
+            if (handData.FingerCount == 2)
+            {
+                twoFingerPoints.Add(new PointR(handData.FingerPoints[0].X, handData.FingerPoints[0].Y, Environment.TickCount));
+                twoFingerPoints.Add(new PointR(handData.FingerPoints[1].X, handData.FingerPoints[1].Y, Environment.TickCount));
+
+              //  this.HandleResize(handData.FingerPoints[0].Location, handData.FingerPoints[1].Location);
+                this.isResizing = true;
+            }
+            else
+            {
+                this.isResizing = false;
+            }
+
+            // recognize gesture when user pause for 10 ms
+            if (isResizing == false && twoFingerPoints.Count > 20)
+            {
+                recognizeGestureUsingNRecognizer(rec, twoFingerPoints);
+                twoFingerPoints.Clear();
+
+            } 
+        }
+
+        public void ResizeTwoHands(HandTracker otherHand)
+        {
+            if (!handData.HasPalmPoint || !otherHand.handData.HasPalmPoint)
+            {
+                return;
+            }
+            if (handData.FingerCount <= 1 && otherHand.handData.FingerCount <= 1)
+            {
+                this.HandleResize(handData.PalmPoint.Value, otherHand.handData.PalmPoint.Value);
+                this.isResizing = true;
+            }
+            else
+            {
+                this.isResizing = false;
+            }
+        }
+
+        private void HandleResize(Point p1, Point p2)
+        {
+            if (isResizing)
+            {
+                hoveredImage.Resize(startDragPoint, startDragPoint2, p1, p2);
+            }
+            startDragPoint = p1;
+            startDragPoint2 = p2;
+        }
+
+        public bool IsOverImage { get { return this.hoveredImage != null; } }
+    }
+}
---
+++ candescentnui-20148/CCT.NUI.Samples/ImageManipulation/ImageForm.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using CCT.NUI.Core;
+using CCT.NUI.HandTracking;
+using CCT.NUI.Visual;
+
+using Recognizer.NDollar;
+
+namespace CCT.NUI.Samples.ImageManipulation
+{
+    public partial class ImageForm : Form
+    {
+        private IList<InteractiveImage> images = new List<InteractiveImage>();
+        private IList<HandTracker> handTracks = new List<HandTracker>();
+
+        private IHandDataSource handDataSource;
+        private HandLayer handLayer;
+
+
+        GeometricRecognizer rec;
+        enum gestureName { TranslateLeft, TranslateRight, RotateRight, RotateLeft,ZoomIn,ZoomOut };
+        gestureName gesture = gestureName.TranslateLeft;
+        int numOfGestures =6;
+        public ImageForm()
+        {
+            InitializeComponent();
+
+           // this.images = new ImageLoader(100, 100, this.Width).LoadImages();
+            this.Paint += new PaintEventHandler(ImageForm_Paint);
+            this.FormClosing += new FormClosingEventHandler(ImageForm_FormClosing);
+
+            rec = new GeometricRecognizer();
+
+            //load all template for each gesture
+            for (int i = 1; i <= 10; i++)
+            {
+                rec.LoadGesture(@"C:\Users\ahmed nady\Documents\" + gesture + "" + i + ".xml");
+                if (i == 10 && numOfGestures > 0)
+                {
+                   
+                    i = 1;
+                    numOfGestures--;
+                    gesture ++;
+                }
+            }
+            ////rec.LoadGesture(@"C:\Users\ahmed nady\Documents\TranslateRightLine.xml");
+            ////rec.LoadGesture(@"C:\Users\ahmed nady\Documents\RotateLeftCircle.xml");
+            ////rec.LoadGesture(@"C:\Users\ahmed nady\Documents\RotateRightPI.xml");
+            ////rec.LoadGesture(@"C:\Users\ahmed nady\Documents\ZoomInCaret.xml");
+            ////rec.LoadGesture(@"C:\Users\ahmed nady\Documents\ZoomOutCaret.xml");
+
+           // rec.LoadGesture(@"C:\Users\ahmed nady\Documents\ZoomIn2Fingers.xml");
+          //  rec.LoadGesture(@"C:\Users\ahmed nady\Documents\ZoomOut2Fingers.xml");
+            
+        }
+
+        public ImageForm(IHandDataSource handDataSource)
+            : this()
+        {
+            this.handDataSource = handDataSource;
+            this.handDataSource.NewDataAvailable += new NewDataHandler<HandCollection>(handDataSource_NewDataAvailable);
+            this.handLayer = new HandLayer(this.handDataSource);
+            this.handLayer.ShowConvexHull = true;
+        }
+
+
+
+        private void ResetHands()
+        {
+            this.handTracks.Clear();
+            this.Invalidate();
+        }
+
+        private void UpdateHandTrackData(HandCollection handData)
+        {
+            foreach (var newHand in handData.Hands.Where(h => !this.handTracks.Any(t => t.Id == h.Id)))
+            {
+                this.handTracks.Add(new HandTracker(newHand));
+            }
+            foreach (var handTrack in this.handTracks.ToList())
+            {
+                var newHand = handData.Hands.Where(h => h.Id == handTrack.Id).FirstOrDefault();
+                if (newHand == null)
+                {
+                    this.handTracks.Remove(handTrack);
+                }
+                else
+                {
+                    if (!newHand.HasPalmPoint)
+                    {
+                        continue;
+                    }
+                    handTrack.SetHandData(newHand);
+
+                    handTrack.HandleTranslation(rec);
+
+                    //var hoveredImage = this.images.Where(i => this.ImageContains(i, newHand.PalmPoint.Value)).LastOrDefault();
+                    //if (hoveredImage != null)
+                    //{
+                    //    MoveImageToFront(hoveredImage);
+                    //    handTrack.HandleTranslation(hoveredImage, this.handLayer.ZoomFactor);
+                    //}
+                }
+            }
+        }                             
+            
+        private void MoveImageToFront(InteractiveImage image)
+        {
+            this.images.Remove(image);
+            this.images.Add(image);
+        }
+
+        private bool ImageContains(InteractiveImage image, Point center)
+        {
+            return image.Area.Contains(MapPoint(center, this.handDataSource.Size));
+        } 
+
+        private void UnhoverImages()
+        {
+            foreach (var localImage in this.images)
+            {
+                localImage.Hovered = false;
+            }
+        }
+
+        void handDataSource_NewDataAvailable(HandCollection handData)
+        {
+            if (handData.IsEmpty)
+            {
+                this.ResetHands();
+                return;
+            }
+
+            this.UnhoverImages();
+            this.UpdateHandTrackData(handData);
+
+            var handsOverImages = this.handTracks.Where(h => h.IsOverImage);
+            foreach (var handTrack in handsOverImages)
+            {
+                this.HandleTwoHandedActions(handTrack);
+            }
+
+            this.Invalidate();
+        }
+
+        private void HandleTwoHandedActions(HandTracker handTrack)
+        {
+            var otherHand = this.handTracks.Where(h => h != handTrack && h.HoveredImage == handTrack.HoveredImage).FirstOrDefault();
+            if (otherHand == null)
+            {
+                handTrack.performLastGestureNumberOfTimes();
+                //handTrack.ResizeSingleHand(rec);
+            }
+            else
+            {
+                handTrack.ResizeTwoHands(otherHand);
+            }
+        }
+
+        private Point MapPoint(Point point, IntSize originalSize)
+        {
+            return new Point(point.X / originalSize.Width * this.ClientSize.Width, point.Y / originalSize.Height * this.ClientSize.Height, 0);
+        }
+
+        private System.Drawing.Point MapToScreen(Point point, CCT.NUI.Core.Size originalSize)
+        {
+            int border = 50;
+            return new System.Drawing.Point(-border + (int)(point.X / originalSize.Width * (Screen.PrimaryScreen.Bounds.Width + 2 * border)), - border + (int)(point.Y / originalSize.Height * (Screen.PrimaryScreen.Bounds.Height + 2 * border)));
+        }
+
+        private void ResetImages()
+        {
+            foreach (var image in this.images.ToList())
+            {
+                image.Reset();
+            }
+            this.Invalidate();
+        }
+
+        void ImageForm_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var image in this.images.ToList())
+            {
+                image.Draw(e.Graphics);
+            }
+            this.handLayer.SetTargetSize(this.Size);
+            this.handLayer.SetZoomHandFactor(1 / this.handLayer.ZoomFactor);
+            this.handLayer.Paint(e.Graphics);
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.ResetImages();
+        }
+
+        void ImageForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.handDataSource.Stop();
+        }
+    }
+}
